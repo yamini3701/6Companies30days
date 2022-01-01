2bfada1044b2c0d4fb1edbae6c6022eeada1900f
yamini3701 <yamini171117@gmail.com>
2022-01-01 13:19:11 +0530

    #ReviseWithArsh Que1

---
+++ 1-GroupAnagrams.cpp
@@ -0,0 +1,60 @@
+//#6Companies30Days Challenge GoldmanSachs-1 (Print Anagrams Together)
+
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        map <string,vector<string>> m;
+        
+        for(int i=0; i<string_list.size(); i++){
+            string s = string_list[i];
+            sort(s.begin(), s.end());
+            m[s].push_back(string_list[i]);
+        }
+        
+        vector<vector<string>> v; //2d array
+        
+        for(auto x:m){
+            v.push_back(x.second);
+        }
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
