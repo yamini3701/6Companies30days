b8e6ff321a11535754d548af10d31a76ef8c239d
yamini3701 <yamini171117@gmail.com>
2022-01-02 12:20:51 +0530

    #ReviseWithArsh Que7

---
+++ 7-FindPositionOfMthItem.cpp
@@ -0,0 +1,35 @@
+//#6Companies30Days Challenge GoldmanSachs-7 (Find Position of Mth Item)
+
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        if(M <= N-K+1){
+            return M+K-1;
+        }
+        M=M-(N-K+1);
+        return(M%N==0) ? N:(M%N);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
