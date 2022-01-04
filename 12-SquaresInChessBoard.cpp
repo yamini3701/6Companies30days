406c07eeb9e39f2ec16adbf5c53c0d4f9489f5a4
yamini3701 <yamini171117@gmail.com>
2022-01-04 21:40:54 +0530

    #ReviseWithArsh Que-12

---
+++ 12-SquaresInChessBoard.cpp
@@ -0,0 +1,30 @@
+//#6Companies30Days Challenge GoldmanSachs-12 (SquaresInN*NchessBoard)
+
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long n) {
+        long long int ans = (n*(n+1)*(2*n+1))/6;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
