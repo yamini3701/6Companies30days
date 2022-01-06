af992652edfe1000656da85c45035692a52d6784
yamini3701 <yamini171117@gmail.com>
2022-01-06 18:06:05 +0530

    #ReviseWithArsh Amazon Que-8

---
+++ Day 6-10 [Amazon]/8-CountWaysToNthStair.cpp
@@ -0,0 +1,45 @@
+//#6Companies30Days Challenge Amazon-8 (CountWaysToNthStair)
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        if(m==1){
+            return 1;
+        }
+        if(m%2==0){
+            return countWays(m-1)+1;
+        }
+        else{
+            return countWays(m-1);
+        }
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends
