13aa95072e6b461db80f9b3fbd420d5730d85446
yamini3701 <yamini171117@gmail.com>
2022-01-08 15:02:23 +0530

    #ReviseWithArsh Amazin Que-12

---
+++ Day 6-10 [Amazon]/12-ColumnNameFromGivenColumnNumber.cpp
@@ -0,0 +1,35 @@
+//#6Companies30Days Challenge Amazon-12 (Column name from a given column number)
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        string result = "";
+        while(n){
+            char c = 'A' + (n-1)%26;
+            result = c + result;
+            n = (n-1)/26;
+        }
+        return result;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+  // } Driver Code Ends
