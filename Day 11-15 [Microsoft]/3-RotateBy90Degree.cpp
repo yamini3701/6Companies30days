c663ebd7a05d58845baaed69aa19b4284cc332c6
yamini3701 <yamini171117@gmail.com>
2022-01-11 13:08:08 +0530

    #ReviseWithArsh Microsoft Que-3

---
+++ Day 11-15 [Microsoft]/3-RotateBy90Degree.cpp
@@ -0,0 +1,64 @@
+//#6Companies30Days Challenge Microsoft-3 (Rotate by 90 Degree)
+
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+void rotate (vector<vector<int> >& matrix);
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/* matrix : given input matrix, you are require 
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& matrix)
+{
+    int n=matrix.size();
+    for(int i=0; i<n ; i++){
+        for(int j=i ; j<n ; j++){
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    
+    int i=0;
+    int j=n-1;
+    
+    while(i<j){
+        swap(matrix[i], matrix[j]);
+        i++;
+        j--;
+    }
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t; 
+    while(t--)
+    {
+        int n;
+        cin>>n; 
+        vector<vector<int> > matrix(n);
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].resize(n);
+            for(int j=0; j<n; j++)
+                cin>>matrix[i][j]; 
+        }
+        rotate(matrix);
+        for (int i = 0; i < n; ++i)
+        {
+            for(int j=0; j<n; j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
