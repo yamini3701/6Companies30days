4e4ae2f9789b164578b70919b8573a90feb72efb
yamini3701 <yamini171117@gmail.com>
2022-01-15 13:59:06 +0530

    #ReviseWithArsh Microsoft Que-5

--- Day 11-15 [Microsoft]/5-StockSpanProblem.cpp
+++ Day 11-15 [Microsoft]/5-StockSpanProblem.cpp
@@ -8,34 +8,40 @@ using namespace std;
  // } Driver Code Ends
 
 
-
 class Solution
 {
     public:
     //Function to calculate the span of stock’s price for all n days.
     vector <int> calculateSpan(int price[], int n)
     {
-      stack<pair<int, int>> st;
-      st.push({price[0], 1});
-      vector<int> ans(n, 1);
-      
-      int i=1;
-      while(i<n)
-      {
-          int cur=price[i];
-          int c=0;
-          while(!st.empty() && st.top().first<=cur)
-          {
-              c+=st.top().second;
-              st.pop();
-          }
-          ans[i]+=c;
-          st.push({cur, ans[i]});
-          i++;
-      }
-      
-      return ans;
-   }
+       int i;
+       vector<int> v;
+       stack<pair<int,int>> s;
+       for( i=0 ; i<n ; i++){
+           if(s.size()==0){
+               v.push_back(-1);
+           }
+           else if(s.size()>0 && s.top().first>price[i]){
+               v.push_back(s.top().second);
+           }
+           else if(s.size()>0 && s.top().first<price[i]){
+               while(s.size()>0 && s.top().first<price[i]){
+                   s.pop();
+               }
+               if(s.size()==0){
+                   v.push_back(-1);
+               }
+               else{
+                   v.push_back(s.top().second);
+               }
+           }
+           s.push({price[i],i});
+       }
+       for(int i=0 ; i<v.size() ; i++){
+           v[i]=i-v[i];
+       }
+       return v;
+    }
 };
 
 
